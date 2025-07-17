b2a03be323a1db81d5ed53b9369eb5af83822801
Rov_Waff <2710182206@qq.com>
2025-07-17 11:10:47 +0800

    配置请求

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
+import requests,json,pprint
+
+
+def do_request(api_key):
+    r=requests.request(
+        method="POST",
+        url="https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
+        headers={"Authorization": api_key,"Content-Type":"applitaction/json"},
+        data=json.dumps({
+            "model":"qwen-plus",
+            "messages": [
+                {"role": "system", "content": "你是一个初中生写作文助手，用户将会给你作文的主题和字数要求，请根据用户的要求写出一篇初中满分作文，不许使用Markdown和emoji"},
+                {"role": "user", "content": "你是谁？"},
+            ]
+        }),
+    )
+    return json.loads(r.content)
+
 def main():
-    print("Hello from textgenerator!")
+    api_key = input("输入你的阿里百炼 api-key: ")
+    pprint.pprint(do_request(api_key))
 
 
 if __name__ == "__main__":
