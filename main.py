05c7b7cefb818aa739e59b8a08ffaef3767ca663
Rov_Waff <2710182206@qq.com>
2025-07-17 11:24:54 +0800

    p:增加输入

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import requests,json,pprint
 
 
-def do_request(api_key):
+def do_request(api_key,subject,n_words):
     r=requests.request(
         method="POST",
         url="https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
         headers={"Authorization": api_key,"Content-Type":"applitaction/json"},
         data=json.dumps({
-            "model":"qwen-plus",
+            "model":"deepseek-r1",
             "messages": [
                 {"role": "system", "content": "你是一个初中生写作文助手，用户将会给你作文的主题和字数要求，请根据用户的要求写出一篇初中满分作文，不许使用Markdown和emoji"},
-                {"role": "user", "content": "你是谁？"},
+                {"role": "user", "content": f"请写一篇主题为{subject}的作文，字数在{n_words}左右"},
             ]
         }),
     )
@@ -18,7 +18,10 @@ def do_request(api_key):
 
 def main():
     api_key = input("输入你的阿里百炼 api-key: ")
-    pprint.pprint(do_request(api_key))
+    subject=input("请输入主题: ")
+    n_words=int(input("请输入字数: "))
+    print("AI生成中...")
+    print(do_request(api_key,subject,n_words)['choices'][0]['message']['content'])
 
 
 if __name__ == "__main__":
