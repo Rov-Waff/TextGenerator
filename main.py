90292ad55c3a8a2c22fb9df6173c6426047fc912
Rov_Waff <2710182206@qq.com>
2025-07-17 11:41:08 +0800

    a:规定文体和额外要求

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests,json,pprint
 ARTICLE_TYPE={1:"记叙文",2:"议论文",3:"说明文"}
 
-def do_request(api_key,subject,n_words,article_type):
+def do_request(api_key,subject,n_words,article_type,other_needs):
     r=requests.request(
         method="POST",
         url="https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions",
@@ -10,7 +10,7 @@ def do_request(api_key,subject,n_words,article_type):
             "model":"deepseek-r1",
             "messages": [
                 {"role": "system", "content": f"你是一个写作文助手，用户将会给你作文的主题和字数要求，请根据用户的要求写出一篇满分{article_type}，不许使用Markdown和emoji,要求写标题"},
-                {"role": "user", "content": f"请写一篇主题为{subject}的作文，字数在{n_words}左右,额外要求:"},
+                {"role": "user", "content": f"请写一篇主题为{subject}的作文，字数在{n_words}左右,额外要求:{other_needs}"},
             ]
         }),
     )
@@ -18,8 +18,13 @@ def do_request(api_key,subject,n_words,article_type):
 
 def main():
     api_key = input("输入你的阿里百炼 api-key: ")
-    subject=ARTICLE_TYPE[input("请输入主题(1.记叙文 2.议论文 3.说明文): ")]
-    article_type=input("请输入文体 :")
+    while True:
+        try:
+            article_type=ARTICLE_TYPE[int(input("请输入文体(1.记叙文 2.议论文 3.说明文): "))]
+            break
+        except:
+            print("输入1或2或3")
+    subject=input("请输入主题: ")
     while(True):
         try:
             n_words=int(input("请输入字数: "))
@@ -27,9 +32,10 @@ def main():
         except:
             print("好歹你输入个数字吧（")
             continue
+    other_needs=input("其他要求: ")
     print("AI生成中...")
     try:
-        print(do_request(api_key,subject,n_words,article_type)['choices'][0]['message']['content'])
+        print(do_request(api_key,subject,n_words,article_type,other_needs)['choices'][0]['message']['content'])
     except:
         print("请求失败，检查你的API-Key")
 
